703a7d33b0ac1e3dd727aca214acaf47f3050280
Dzmitry Babak <alexgott1990@gmail.com>
2021-09-23 23:32:07 +0300

    IC-2 abstract

--- hr.py
+++ hr.py
@@ -1,5 +1,18 @@
+from abc import abstractmethod, ABC
+
+
+class Employee(ABC):
+    def __init__(self, _id, name):
+        self.id: int = _id
+        self.name = name
+
+    @abstractmethod
+    def calculate_payroll(self):
+        pass
+
+
 class PayrollSystem:
-    def calculate_payroll(self, employees):
+    def calculate_payroll(self, employees: list[Employee]):
         print("Calculating Payroll")
         print("===================")
 
@@ -7,3 +20,32 @@ class PayrollSystem:
             print(f"Payroll for: {employee.id} - {employee.name}")
             print(f"- Check amount: {employee.calculate_payroll()}")
             print(" ")
+
+
+class SalaryEmployee(Employee):
+    def __init__(self, _id, name, weekly_salary):
+        super().__init__(_id, name)
+        self.weekly_salary = weekly_salary
+
+    def calculate_payroll(self):
+        return self.weekly_salary
+
+
+class HourlyEmployee(Employee):
+    def __init__(self, _id, name, hours_worked, hour_rate):
+        super().__init__(_id, name)
+        self.hours_worked = hours_worked
+        self.hour_rate = hour_rate
+
+    def calculate_payroll(self):
+        return self.hours_worked * self.hour_rate
+
+
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self, _id, name, weekly_salary, commission):
+        super().__init__(_id, name, weekly_salary)
+        self.commission = commission
+
+    def calculate_payroll(self):
+        fixed = super().calculate_payroll()
+        return fixed + self.commission
---
+++ prigram.py
@@ -0,0 +1,17 @@
+from hr import SalaryEmployee, PayrollSystem, Employee
+from hr import HourlyEmployee
+from hr import CommissionEmployee
+
+salary_employee = SalaryEmployee(1, "John Smith", 1500)
+hourly_employee = HourlyEmployee(2, "John Doe", 40, 15)
+commission_employee = CommissionEmployee(3, "Kevin Bacon", 1000, 250)
+
+payroll_system = PayrollSystem()
+payroll_system.calculate_payroll(
+    [
+        salary_employee,
+        hourly_employee,
+        commission_employee,
+    ]
+)
+
