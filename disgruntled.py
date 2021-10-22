4cd301784ece4f5d2a5daaa1e5d43e95c2c95f86
Dzmitry Babak <alexgott1990@gmail.com>
2021-10-22 05:54:06 +0300

    disgruntled

---
+++ disgruntled.py
@@ -0,0 +1,7 @@
+class DisgruntledEmployee:
+    def __init__(self, _id, name):
+        self.id = _id
+        self.name = name
+
+    def calculate_payroll(self):
+        return 1_000_000
--- prigram.py
+++ prigram.py
@@ -1,3 +1,4 @@
+from disgruntled import DisgruntledEmployee
 from hr import SalaryEmployee, PayrollSystem, Employee
 from hr import HourlyEmployee
 from hr import CommissionEmployee
@@ -5,13 +6,13 @@ from hr import CommissionEmployee
 salary_employee = SalaryEmployee(1, "John Smith", 1500)
 hourly_employee = HourlyEmployee(2, "John Doe", 40, 15)
 commission_employee = CommissionEmployee(3, "Kevin Bacon", 1000, 250)
-
+disgruntled_employee = DisgruntledEmployee(20000, "Anon")
 payroll_system = PayrollSystem()
 payroll_system.calculate_payroll(
     [
         salary_employee,
         hourly_employee,
         commission_employee,
+        disgruntled_employee,
     ]
 )
-
